35b18a80f3ff73687c40331854bc190179995b19
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-09-15 14:33:55 -0700

    Add project files.

---
+++ Fall2021_COMP2084_CourseProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fall2021_COMP2084_CourseProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
