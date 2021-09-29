ba6c85aea3d36cd649458444bd511c9bb42c64a2
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-09-29 15:21:46 -0700

    Changed the site's title and footer

--- Fall2021_COMP2084_CourseProject/Controllers/HomeController.cs
+++ Fall2021_COMP2084_CourseProject/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using Fall2021_COMP2084_CourseProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Fall2021_COMP2084_CourseProject.Controllers
 {
