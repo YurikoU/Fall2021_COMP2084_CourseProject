e3526755a009e9af288a107bcab11ee3b92a68bc
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-09-29 16:58:21 -0700

    Created a new controller called CitiesController

---
+++ Fall2021_COMP2084_CourseProject/Controllers/CitiesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fall2021_COMP2084_CourseProject.Controllers
+{
+    public class CitiesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
