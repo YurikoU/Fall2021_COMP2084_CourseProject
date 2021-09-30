4bcafd47458804a4922b0579e6085b0262ea2a4d
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-09-29 18:46:56 -0700

    Newly created models to store ads for each city

---
+++ Fall2021_COMP2084_CourseProject/Models/Kitchener.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Fall2021_COMP2084_CourseProject.Models
+{
+    public class Kitchener
+    {
+        public int Id { get; set; }
+        public DateTime PostedDate { get; set; }
+        public string AuthorName { get; set; }
+        public string Description { get; set; }
+        public string AuthorPhone { get; set; }
+        public string AuthorEmail { get; set; }
+    }
+}
---
+++ Fall2021_COMP2084_CourseProject/Models/Ottawa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Fall2021_COMP2084_CourseProject.Models
+{
+    public class Ottawa
+    {
+        public int Id { get; set; }
+        public DateTime PostedDate { get; set; }
+        public string AuthorName { get; set; }
+        public string Description { get; set; }
+        public string AuthorPhone { get; set; }
+        public string AuthorEmail { get; set; }
+
+    }
+}
