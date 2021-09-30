bb122de0603bbdeef2a0f6e2216787cff4a998f9
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-09-29 18:28:54 -0700

    Added asp-route-city attribute in the view so the CitiesController can retrieve the input

--- Fall2021_COMP2084_CourseProject/Controllers/CitiesController.cs
+++ Fall2021_COMP2084_CourseProject/Controllers/CitiesController.cs
@@ -11,9 +11,12 @@ namespace Fall2021_COMP2084_CourseProject.Controllers
             return View();
         }
 
-        //Cities/Browse
-        public IActionResult Browse() 
+        //Cities/Browse?city={asp-route-city value}
+        public IActionResult Browse(string city) 
         {
+            //Store the clicked city name
+            ViewBag.city = city;
+
             //Load the view to display
             return View();
         }
