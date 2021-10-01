df08112bc1c79cddaf578800555c1e04e21a9402
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-10-01 12:18:19 -0700

    Set up the database info in appsettings.json

--- Fall2021_COMP2084_CourseProject/Data/ApplicationDbContext.cs
+++ Fall2021_COMP2084_CourseProject/Data/ApplicationDbContext.cs
@@ -7,7 +7,7 @@ namespace Fall2021_COMP2084_CourseProject.Data
     public class ApplicationDbContext : IdentityDbContext
     {
 
-        //Declare global references for all models so they are available widely
+        //Declare global references for all models to interact with the database
         public DbSet<City> Cities { get; set; }
         public DbSet<Post> Posts { get; set; }
 
