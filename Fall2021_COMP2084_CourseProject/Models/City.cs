ff7b10cc2dc4323012fb50e97abedba580d9b1af
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-10-01 11:46:31 -0700

    Changed some comments for models

--- Fall2021_COMP2084_CourseProject/Models/City.cs
+++ Fall2021_COMP2084_CourseProject/Models/City.cs
@@ -7,6 +7,7 @@ namespace Fall2021_COMP2084_CourseProject.Models
         public int Id { get; set; }//Primary key
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter a new city name.")]
+        [MaxLength(50, ErrorMessage = "A city name must be up to 50 characters.")]
         [Display(Name = "City Name")]
         public string Name { get; set; }
     }
--- Fall2021_COMP2084_CourseProject/Models/Post.cs
+++ Fall2021_COMP2084_CourseProject/Models/Post.cs
@@ -7,7 +7,7 @@ namespace Fall2021_COMP2084_CourseProject.Models
     {
         public int Id { get; set; } //Primary key
 
-        [DataType(DataType.Date)] //Only the date will be shown.
+        [DataType(DataType.Date)] //Show only the date
         [Display(Name = "Posted Date")]
         public DateTime PostedDate { get; set; }
 
@@ -15,7 +15,7 @@ namespace Fall2021_COMP2084_CourseProject.Models
 
         [Range(100, 999999, ErrorMessage = "The rent must be between $100 and $999,999")]
         [Display(Name = "Monthly rent (CAD$)")]
-        public int Rent { get; set; }
+        public int Rent { get; set; } //Rent is optional because some rooms' rents are undisclosed.
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "Please describe the room a bit.")]
         [MaxLength(1500, ErrorMessage = "A description must be up to 1,500 characters.")]
@@ -30,15 +30,17 @@ namespace Fall2021_COMP2084_CourseProject.Models
         [Required(AllowEmptyStrings = false, ErrorMessage = "A phone number is required so a user can contact you.")]
         [MaxLength(20, ErrorMessage = "A phone number must be up to 20 characters.")]
         [Phone(ErrorMessage = "Please enter a valid phone number.")] //The input value must be a phone number
+        [DataType(DataType.PhoneNumber)] //Show the data as a phone number
         [Display(Name = "Contact phone number")]
         public string PhoneOnPost { get; set; }
 
         [MaxLength(60, ErrorMessage = "An e-mail address must be up to 60 characters.")]
         [EmailAddress(ErrorMessage = "Please enter a properly formatted e-mail address.")] //The input value must be an email address
+        [DataType(DataType.EmailAddress)]//Show the data as an email address
         [Display(Name = "Contact e-mail address")]
         public string EmailOnPost { get; set; }
 
-        // Represents the parent object so Post can refer to the parent's property (1 City to many Posts)
+        //Represents the parent object so Post can refer to the parent's property (1 City to many Posts)
         //This is called "Navigation Property" by Microsoft
         public City City { get; set; }
     }
