64663d40d8e46b30dabd8094a3291e84f969b3f8
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-10-01 17:04:45 -0700

    Added comments

--- Fall2021_COMP2084_CourseProject/Controllers/PostsController.cs
+++ Fall2021_COMP2084_CourseProject/Controllers/PostsController.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Fall2021_COMP2084_CourseProject.Data;
 using Fall2021_COMP2084_CourseProject.Models;
+/*
+GET : Before clicking the submit button
+POST: After clicking the submit button
+ */
 
 namespace Fall2021_COMP2084_CourseProject.Controllers
 {
@@ -16,13 +18,17 @@ namespace Fall2021_COMP2084_CourseProject.Controllers
 
         public PostsController(ApplicationDbContext context)
         {
+            //Process the database connection
             _context = context;
         }
 
         // GET: Posts
         public async Task<IActionResult> Index()
         {
+            //Access the parent table so the view can automatically show the category name from the parent table
             var applicationDbContext = _context.Posts.Include(p => p.City);
+
+            //Retrieve the data and convert it to the list
             return View(await applicationDbContext.ToListAsync());
         }
 
@@ -48,6 +54,7 @@ namespace Fall2021_COMP2084_CourseProject.Controllers
         // GET: Posts/Create
         public IActionResult Create()
         {
+            //Get the list of Cities and store them in ViewData
             ViewData["CityId"] = new SelectList(_context.Cities, "Id", "Name");
             return View();
         }
@@ -59,10 +66,16 @@ namespace Fall2021_COMP2084_CourseProject.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,PostedDate,Rent,Description,Photo,PhoneOnPost,EmailOnPost,UserId,CityId")] Post post)
         {
+            //Check if the input is valid
             if (ModelState.IsValid)
             {
+                //Add the Post object
                 _context.Add(post);
+
+                //Save the changes on the database connection
                 await _context.SaveChangesAsync();
+
+                //Once done, it will redirect the user to the index page
                 return RedirectToAction(nameof(Index));
             }
             ViewData["CityId"] = new SelectList(_context.Cities, "Id", "Name", post.CityId);
@@ -82,6 +95,7 @@ namespace Fall2021_COMP2084_CourseProject.Controllers
             {
                 return NotFound();
             }
+            //Get the list of Cities and store them in ViewData
             ViewData["CityId"] = new SelectList(_context.Cities, "Id", "Name", post.CityId);
             return View(post);
         }
@@ -98,11 +112,15 @@ namespace Fall2021_COMP2084_CourseProject.Controllers
                 return NotFound();
             }
 
+            //Check if the input is valid
             if (ModelState.IsValid)
             {
                 try
-                {
+                { 
+                    //Update the Post object
                     _context.Update(post);
+
+                    //Save the changes on the database connection
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
@@ -116,6 +134,8 @@ namespace Fall2021_COMP2084_CourseProject.Controllers
                         throw;
                     }
                 }
+
+                //Once done, it will redirect the user to the index page
                 return RedirectToAction(nameof(Index));
             }
             ViewData["CityId"] = new SelectList(_context.Cities, "Id", "Name", post.CityId);
