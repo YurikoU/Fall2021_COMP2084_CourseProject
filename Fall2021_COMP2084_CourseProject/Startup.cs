6206612d54ac42bacf016adc3836fd666dc6e23b
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-11-04 14:12:46 -0700

    Formatted the startup.cs

--- Fall2021_COMP2084_CourseProject/Startup.cs
+++ Fall2021_COMP2084_CourseProject/Startup.cs
@@ -63,6 +63,7 @@ namespace Fall2021_COMP2084_CourseProject
                 });
         }
 
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
