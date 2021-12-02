b235d9ef66d8ab3bcb5d96357799a87f674c30b3
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-12-01 16:43:29 -0800

    Passed EditLoads404IfCityidInvalid() and EditLoads404IfCityidNull()

--- SearchInRoomTests/CitiesControllerTests.cs
+++ SearchInRoomTests/CitiesControllerTests.cs
@@ -1,5 +1,9 @@
+using Fall2021_COMP2084_CourseProject.Controllers;
+using Fall2021_COMP2084_CourseProject.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using System;
 
 namespace SearchInRoomTests
 {
@@ -8,7 +12,26 @@ namespace SearchInRoomTests
     public class CitiesControllerTests
     {
         #region Class level variables and methods
+        private ApplicationDbContext _context;
+        CitiesController controller;
 
+        //[TestInitialize] decorator: runs automatically before each test
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            //Setting-up the options
+            //Create in-memory DB not to connect to external DB
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())//To use UseInMemoryDatabase(), you must have a unique name, eg; using Guid().
+                .Options;
+
+            //Instantiate the DB object
+            _context = new ApplicationDbContext(options);
+
+            //Instantiate the controller with the DB dependency
+            //CitiesController can't be instantiated without an active DB as the controller starts with SQL DB connection before running methods
+            controller = new CitiesController(_context);
+        }
         #endregion
 
 
@@ -18,26 +41,23 @@ namespace SearchInRoomTests
         //Check for null CityId
         public void EditLoads404IfCityidNull()
         {
-            //Arrange(=given values)
-
-
             //Act(=when execution)
+            var result = (ViewResult)controller.Edit(null)
+                .Result;  //Allow you to read the asynchronous task and cast it to a view result, cuz the method(=Edit()) is async.
 
             //Assert(=result) 
-
+            Assert.AreEqual("404", result.ViewName);
         }
 
         //Check for invalid CityId
         [TestMethod]
         public void EditLoads404IfCityidInvalid()
         {
-            //Arrange(=given values)
-
-
             //Act(=when execution)
+            var result = (ViewResult)controller.Edit(999).Result;//DB doesn't have CityId=999
 
             //Assert(=result) 
-
+            Assert.AreEqual("404", result.ViewName);
         }
 
         //Check for returning the correct data model (City obj)
@@ -48,6 +68,7 @@ namespace SearchInRoomTests
 
 
             //Act(=when execution)
+
 
             //Assert(=result) 
 
